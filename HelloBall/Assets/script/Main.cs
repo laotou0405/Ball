8f9c265398e15b8962ee4b97fe8eb0c4173ceb60
zhengkai <89144143@qq.com>
2015-07-09 19:40:28 +0800

    HelloBall

---
+++ HelloBall/Assets/script/Main.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Main : MonoBehaviour {
+
+	// Use this for initialization
+	private cube floor;
+	void Start () {
+		floor = new cube();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI(){
+
+
+	}
+}
---
+++ HelloBall/Assets/script/cube.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cube{
+
+	// Use this for initialization
+	private GameObject floor;
+	public cube () {
+
+		floor = GameObject.CreatePrimitive(PrimitiveType.Cube);
+		floor.transform.position = new Vector3(0,0,0);
+		floor.GetComponent<BoxCollider>();
+		floor.GetComponent<BoxCollider>().isTrigger = true;
+		floor.GetComponent<BoxCollider>().size = new Vector2(0.1f,0.1f);
+	}
+	
+}
