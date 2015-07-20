730143f6e220a29381fd730da46cbf0f13d960c0
laotou <88631613@qq.com>
2015-07-20 22:07:16 +0800

    提交资源

---
+++ Music/Assets/script/View/UiManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UiManager : MonoBehaviour {
+
+	// Use this for initialization
+    private Dictionary<string,GameObject> uiDic;
+
+    void Awake()
+    {
+
+
+    }
+
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Music/Assets/script/main.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class main : MonoBehaviour {
+
+	// Use this for initialization
+	UISprite bg;
+    UIButton startBtn;
+
+    void Start()
+    {
+        bg = (UISprite) gameObject.AddComponent("UISprite");
+        bg.width = 640;
+        bg.height = 500;
+        UIAtlas atlas = (UIAtlas)Resources.Load("music", typeof(UIAtlas));
+
+        bg.atlas = atlas;
+
+        bg.spriteName = "bg";
+
+
+        UITexture texture = (UITexture) gameObject.AddComponent("UITexture");
+        texture.color = Color.red;
+
+    }
+
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+
+    void OnGUI()
+    {
+
+    }
+
+}
